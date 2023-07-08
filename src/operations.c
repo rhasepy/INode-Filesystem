60b904f29f469707adbb3efa0baf678ff1ad5778
rhasepy <ozannyesiller@gmail.com>
2023-07-08 23:47:00 +0300

    rm import export remain

--- src/operations.c
+++ src/operations.c
@@ -5,6 +5,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Helper function to find the directory inode index given the path
+int find_parent_directory(file_system* fs, char* path) {
+    // Separate the path into directory names
+    char* token = strtok(path, "/");
+    int parent_inode_num = fs->root_node;
+
+    // Traverse the path to find the parent directory
+    while (token != NULL) {
+        int found = 0;
+        for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
+            int inode_num = fs->inodes[parent_inode_num].direct_blocks[i];
+            if (inode_num != -1) {
+                inode* curr_inode = &fs->inodes[inode_num];
+                if (strcmp(curr_inode->name, token) == 0) {
+                    if (curr_inode->n_type == directory) {
+                        parent_inode_num = inode_num;
+                        found = 1;
+                        break;
+                    } else {
+                        printf("'%s' is not a directory.\n", token);
+                        return -1;
+                    }
+                }
+            }
+        }
+
+        if (!found) {
+            printf("Directory '%s' not found.\n", token);
+            return -1;
+        }
+
+        token = strtok(NULL, "/");
+    }
+
+    return parent_inode_num;
+}
+
 char*
 reverse_str(char *str)
 {
@@ -289,195 +326,215 @@ fs_list(file_system* fs, char* path) {
     return result;
 }
 
-int
-fs_writef(file_system* fs, char* filename, char* text) {
-    // Get the parent directory inode number
-    int parent_inode_number = fs->root_node;
-    char* token = strtok(filename, "/");
-    while (token != NULL) {
-        // Check if the parent inode is a directory
-        if (fs->inodes[parent_inode_number].n_type != directory) {
-            printf("Parent is not a directory.\n");
+int fs_writef(file_system* fs, char* filepath, char* text) {
+    // Separate the path and filename
+    char* path = strdup(filepath);
+    char* filename = strrchr(path, '/');
+    if (filename != NULL) {
+        *filename = '\0';  // Terminate the path string at the last '/'
+        filename++;  // Skip the '/' character
+    } else {
+        // No directory path specified, use the root directory
+        filename = path;
+        path = NULL;
+    }
+
+    // Find the parent directory inode
+    int parent_inode_num;
+    if (path == NULL) {
+        parent_inode_num = fs->root_node;
+    } else {
+        parent_inode_num = find_parent_directory(fs, path);
+        if (parent_inode_num == -1) {
+            printf("Directory '%s' not found.\n", path);
+            free(path);
             return -1;
         }
+    }
 
-        // Check if the directory exists
-        int child_inode_number =-1;
-        for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-            if (fs->inodes[parent_inode_number].direct_blocks[i] != -1 &&
-                strcmp(fs->inodes[fs->inodes[parent_inode_number].direct_blocks[i]].name, token) == 0) {
-                child_inode_number = fs->inodes[parent_inode_number].direct_blocks[i];
+    // Find the inode of the file
+    int file_inode_num = -1;
+    inode* parent_inode = &fs->inodes[parent_inode_num];
+    for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
+        int inode_num = parent_inode->direct_blocks[i];
+        if (inode_num != -1) {
+            inode* curr_inode = &fs->inodes[inode_num];
+            if (strcmp(curr_inode->name, filename) == 0) {
+                file_inode_num = inode_num;
                 break;
             }
         }
-
-        // Directory does not exist
-        if (child_inode_number == -1) {
-            printf("Directory does not exist.\n");
-            return -1;
-        }
-
-        parent_inode_number = child_inode_number;
-        token = strtok(NULL, "/");
     }
 
-    // Check if the file exists in the parent directory
-    int file_inode_number = -1;
+    if (file_inode_num == -1) {
+        printf("File '%s' does not exist.\n", filepath);
+        free(path);
+        return -1;
+    }
+
+    inode* file_inode = &fs->inodes[file_inode_num];
+    if (file_inode->n_type != reg_file) {
+        printf("'%s' is not a regular file.\n", filepath);
+        return -1;
+    }
+
+    // Find the last used data block index
+    int last_block_idx = -1;
     for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-        if (fs->inodes[parent_inode_number].direct_blocks[i] != -1 &&
-            strcmp(fs->inodes[fs->inodes[parent_inode_number].direct_blocks[i]].name, filename) == 0) {
-            file_inode_number = fs->inodes[parent_inode_number].direct_blocks[i];
+        if (file_inode->direct_blocks[i] == -1) {
+            last_block_idx = i - 1;
             break;
         }
     }
 
-    // File does not exist
-    if (file_inode_number == -1) {
-        printf("File does not exist.\n");
-        return -1;
+    // Calculate the total size of the text to be appended
+    int total_text_len = strlen(text);
+    int chars_written = 0;
+
+    // Iterate over the existing data blocks
+    for (int i = 0; i <= last_block_idx; i++) {
+        int block_num = file_inode->direct_blocks[i];
+        data_block* block = &fs->data_blocks[block_num];
+        int remaining_space = BLOCK_SIZE - block->size;
+
+        if (remaining_space > 0) {
+            // There is remaining space in the block
+            int text_len = (total_text_len > remaining_space) ? remaining_space : total_text_len;
+
+            memcpy(block->block + block->size, text, text_len);
+            block->size += text_len;
+            chars_written += text_len;
+
+            text += text_len;
+            total_text_len -= text_len;
+
+            if (total_text_len == 0) {
+                // All text has been written
+                return chars_written;
+            }
+        }
     }
 
-    // Find a free data block for writing the text
-    int new_data_block_number = find_free_data_block(fs);
-    if (new_data_block_number == -1) {
-        printf("No free data block available.\n");
-        return -1;
+    // If there is more text to be written, allocate new data blocks
+    while (total_text_len > 0) {
+        // Find a free data block
+        int new_block_num = find_free_data_block(fs);
+        if (new_block_num == -1) {
+            printf("File '%s' is full.\n", filepath);
+            return -2;
+        }
+
+        // Update the file inode with the new data block
+        if (last_block_idx == DIRECT_BLOCKS_COUNT - 1) {
+            printf("File '%s' is full.\n", filepath);
+            return -2;
+        }
+
+        last_block_idx++;
+        file_inode->direct_blocks[last_block_idx] = new_block_num;
+
+        // Write as much text as possible to the new block
+        data_block* new_block = &fs->data_blocks[new_block_num];
+        int remaining_space = BLOCK_SIZE;
+        int copy_len = (total_text_len > remaining_space) ? remaining_space : total_text_len;
+
+        memcpy(new_block->block, text, copy_len);
+        new_block->size = copy_len;
+        chars_written += copy_len;
+
+        text += copy_len;
+        total_text_len -= copy_len;
     }
 
-    // Copy the text to the data block
-    strcpy((char*)fs->data_blocks[new_data_block_number].block, text);
-    fs->data_blocks[new_data_block_number].size = strlen(text);
-
-    // Update the file inode with the new data block
-    fs->inodes[file_inode_number].direct_blocks[0] = new_data_block_number;
-
-    printf("Data written to file successfully.\n");
-    return 0;
+    return chars_written;
 }
 
 uint8_t*
-fs_readf(file_system* fs, char* filename, int* file_size) {
-    // Get the parent directory inode number
-    int parent_inode_number = fs->root_node;
-    char* token = strtok(filename, "/");
-    while (token != NULL) {
-        // Check if the parent inode is a directory
-        if (fs->inodes[parent_inode_number].n_type != directory) {
-            printf("Parent is not a directory.\n");
+fs_readf(file_system* fs, char* filepath, int* file_size) {
+    // Separate the path and filename
+    char* path = strdup(filepath);
+    char* filename = strrchr(path, '/');
+    if (filename != NULL) {
+        *filename = '\0';  // Terminate the path string at the last '/'
+        filename++;  // Skip the '/' character
+    } else {
+        // No directory path specified, use the root directory
+        filename = path;
+        path = NULL;
+    }
+
+    // Find the parent directory inode
+    int parent_inode_num;
+    if (path == NULL) {
+        parent_inode_num = fs->root_node;
+    } else {
+        parent_inode_num = find_parent_directory(fs, path);
+        if (parent_inode_num == -1) {
+            printf("Directory '%s' not found.\n", path);
+            free(path);
             return NULL;
         }
+    }
 
-        // Check if the directory exists
-        int child_inode_number = -1;
-        for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-            if (fs->inodes[parent_inode_number].direct_blocks[i] != -1 &&
-                strcmp(fs->inodes[fs->inodes[parent_inode_number].direct_blocks[i]].name, token) == 0) {
-                child_inode_number = fs->inodes[parent_inode_number].direct_blocks[i];
+    // Find the inode of the file
+    int file_inode_num = -1;
+    inode* parent_inode = &fs->inodes[parent_inode_num];
+    for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
+        int inode_num = parent_inode->direct_blocks[i];
+        if (inode_num != -1) {
+            inode* curr_inode = &fs->inodes[inode_num];
+            if (strcmp(curr_inode->name, filename) == 0) {
+                file_inode_num = inode_num;
                 break;
             }
         }
-
-        // Directory does not exist
-        if (child_inode_number == -1) {
-            printf("Directory does not exist.\n");
-            return NULL;
-        }
-
-        parent_inode_number = child_inode_number;
-        token = strtok(NULL, "/");
     }
 
-    // Check if the file exists in the parent directory
-    int file_inode_number = -1;
+    if (file_inode_num == -1) {
+        printf("File '%s' does not exist.\n", filepath);
+        free(path);
+        return NULL;
+    }
+
+    inode* file_inode = &fs->inodes[file_inode_num];
+    if (file_inode->n_type != reg_file) {
+        printf("'%s' is not a regular file.\n", filename);
+        return NULL;
+    }
+
+    // Calculate the file size
+    *file_size = 0;
     for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-        if (fs->inodes[parent_inode_number].direct_blocks[i] != -1 &&
-            strcmp(fs->inodes[fs->inodes[parent_inode_number].direct_blocks[i]].name, filename) == 0) {
-            file_inode_number = fs->inodes[parent_inode_number].direct_blocks[i];
-            break;
+        int block_num = file_inode->direct_blocks[i];
+        if (block_num != -1) {
+            data_block* block = &fs->data_blocks[block_num];
+            *file_size += block->size;
         }
     }
 
-    // File does not exist
-    if (file_inode_number == -1) {
-        printf("File does not exist.\n");
+    // Allocate memory for the buffer
+    uint8_t* buffer = (uint8_t*)malloc(*file_size);
+    if (buffer == NULL) {
+        printf("Memory allocation failed.\n");
         return NULL;
     }
 
-    // Get the data block number for the file
-    int data_block_number = fs->inodes[file_inode_number].direct_blocks[0];
-
-    // Check if the data block exists
-    if (data_block_number == -1) {
-        printf("Data block does not exist.\n");
-        return NULL;
+    // Read the file into the buffer
+    uint8_t* ptr = buffer;
+    for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
+        int block_num = file_inode->direct_blocks[i];
+        if (block_num != -1) {
+            data_block* block = &fs->data_blocks[block_num];
+            memcpy(ptr, block->block, block->size);
+            ptr += block->size;
+        }
     }
 
-    // Retrieve the file size
-    *file_size = fs->data_blocks[data_block_number].size;
-
-    // Return a pointer to the file data block
-    return fs->data_blocks[data_block_number].block;
+    return buffer;
 }
 
 int
 fs_rm(file_system* fs, char* path) {
-    // Get the parent directory inode number
-    int parent_inode_number = fs->root_node;
-    char* token = strtok(path, "/");
-    while (token != NULL) {
-        // Check if the parent inode is a directory
-        if (fs->inodes[parent_inode_number].n_type != directory) {
-            printf("Parent is not a directory.\n");
-            return -1;
-        }
-
-        // Check if the directory exists
-        int child_inode_number = -1;
-        for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-            if (fs->inodes[parent_inode_number].direct_blocks[i] != -1 &&
-                strcmp(fs->inodes[fs->inodes[parent_inode_number].direct_blocks[i]].name, token) == 0) {
-                child_inode_number = fs->inodes[parent_inode_number].direct_blocks[i];
-                break;
-            }
-        }
-
-        // Directory does not exist
-        if (child_inode_number == -1) {
-            printf("Directory does not exist.\n");
-            return -1;
-        }
-
-        parent_inode_number = child_inode_number;
-        token = strtok(NULL, "/");
-    }
-
-    // Get the inode number to remove
-    int inode_number = fs->inodes[parent_inode_number].direct_blocks[0];
-
-    // Check if the inode is a file
-    if (fs->inodes[inode_number].n_type != reg_file) {
-        printf("Cannot remove a non-file.\n");
-        return -1;
-    }
-
-    // Free the data block
-    int data_block_number = fs->inodes[inode_number].direct_blocks[0];
-    fs->free_list[data_block_number] = 1;
-    fs->s_block->free_blocks++;
-
-    // Clear the inode
-    memset(&fs->inodes[inode_number], 0, sizeof(inode));
-
-    // Update the parent directory entry
-    for (int i = 0; i < DIRECT_BLOCKS_COUNT; i++) {
-        if (fs->inodes[parent_inode_number].direct_blocks[i] == inode_number) {
-            fs->inodes[parent_inode_number].direct_blocks[i] = -1;
-            fs->inodes[parent_inode_number].size--;
-            break;
-        }
-    }
-
-    printf("File removed successfully.\n");
     return 0;
 }
 
